9dbad422397c88de11d452df43275b999fd7df6f
Christoph Labacher <christoph.labacher@gmail.com>
2014-10-01 00:14:55 +0200

    Code-Splitansicht hinzugefügt

--- scripts/linenumbers.js
+++ scripts/linenumbers.js
@@ -6,4 +6,12 @@ $(document).ready(function(){
 	    	 $(this).parent().parent().find(".lines").append(i+1 + "<br>");
 	     }
 	});
+	
+	$(".code-snippet").each(function(){
+	    var height = $(this).height() - $(this).find("cite").height();
+	    $(this).find(".lines").outerHeight(height);
+	  	$(this).find("code").outerHeight(height);
+	   	$(this).find(".result").outerHeight(height);
+	    
+	});
 });
