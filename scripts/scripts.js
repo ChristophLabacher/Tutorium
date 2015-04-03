35b4c14f5495e04176cafc87bbd6685faaee0ff9
Christoph Labacher <christoph.labacher@gmail.com>
2015-04-03 17:55:40 +0200

    Erster Inhalt eingepflegt

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,26 +1,29 @@
 $(document).ready(function()	{
+	// Click on a topic leads to the topic
+	$("ul.main ul li").click(function()	{
+		if ($(this).attr("source"))	{
+			var source = "content/";
+			source += $(this).closest("ul.sub").attr("source") + "/";
+			source += $(this).attr("source");
+			source += ".html";
+			
+			$.get(source, function( data ) {
+				$("main article").html( data );
+				lineNumbers();
+			});
+			
+			$("ul.sub").find(".selected").removeClass("selected");
+			$(this).addClass("selected");
+			
+			location.hash = "#/" + $(this).closest("ul.sub").attr("source") + "/" + $(this).attr("source");
+		}
+	});
+	
+	// Click on category leads to its introduction
 	$("ul.main > li").click(function()	{
 		$("ul.main > li").removeClass("selected");
 		$(this).addClass("selected");
-	});
-	
-	$("ul.main ul li").click(function()	{
-		var source = "content/";
-		source += $(this).closest("ul.sub").attr("source") + "/";
-		source += $(this).attr("source");
-		source += ".html";
 		
-		$.get(source, function( data ) {
-			$("main article").html( data );
-		});
-		
-		$("ul.sub").find(".selected").removeClass("selected");
-		$(this).addClass("selected");
-		
-		location.hash = "#/" + $(this).closest("ul.sub").attr("source") + "/" + $(this).attr("source");
-	});
-	
-	$("ul.main > li").click(function()	{
 		var source = "content/";
 		source += $(this).next("ul.sub").attr("source") + "/";
 		source += "einfuehrung";
@@ -28,6 +31,7 @@ $(document).ready(function()	{
 		
 		$.get(source, function( data ) {
 			$("main article").html( data );
+			lineNumbers();
 		});
 		
 		$("ul.sub").find(".selected").removeClass("selected");
@@ -36,6 +40,7 @@ $(document).ready(function()	{
 		location.hash = "#/" + $(this).next("ul.sub").attr("source") + "/einfuehrung";
 	});
 		
+	// Root is meta/introduction else URL-Routing
 	if (location.hash == "")	{
 		location.hash = "#/meta/einfuehrung";	
 	
@@ -51,12 +56,34 @@ $(document).ready(function()	{
 		
 		$.get(source, function( data ) {
 			$("main article").html( data );
+			lineNumbers();
 		});
 		
+		$("ul.main li").removeClass("selected");
+		$("ul.sub[source=" + path[1] + "]").prev("li").addClass("selected");
 		var i = "ul.sub[\"source\"=\"" + path[1] + "\"]";
 		$("ul.sub[source=" + path[1] + "]").find(".selected").removeClass("selected");
 		$("ul.sub[source=" + path[1] + "]").find("[source=" + path[2] + "]").addClass("selected");
-
 	}
 	
+
+	
 });
+
+function lineNumbers()	{
+	$("code pre").each(function(){
+	     var lines = $(this).html().split(/\n/).length;
+	
+	     for (i = 0; i < lines; i++)	{
+	    	 $(this).closest(".code-snippet").find(".lines").append(i+1 + "<br>");
+	     }
+	});
+	
+	$(".code-snippet").each(function(){
+	    var height = $(this).outerHeight() - $(this).find("cite").height();
+	    $(this).children(".lines").outerHeight(height);
+	  	$(this).children("code").outerHeight(height);
+	   	$(this).children(".result").outerHeight(height);
+	    
+	});	
+}
