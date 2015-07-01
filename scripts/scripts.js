a8b685b2cd6c2c833b45b1bdc1331d319cfab982
Christoph Labacher <christoph.labacher@gmail.com>
2015-07-01 22:47:39 +0200

    Added Google Analytics to new page

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,11 @@
+ (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+ })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ ga('create', 'UA-55884850-1', 'auto');
+ ga('send', 'pageview');
+
 $(document).ready(function()	{
 	// Click on a topic leads to the topic
 	$("ul.main ul li").click(function()	{
@@ -91,4 +99,6 @@ function lineNumbers()	{
 	   	$(this).children(".result").outerHeight(height);
 	    
 	});	
+	
+	
 }
